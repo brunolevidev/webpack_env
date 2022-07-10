c3dd6f66e55138faa6b8d76c7377efbaca0b9957
Bruno Velazquez <contacto.bruno.velazquez@gmail.com>
2022-07-10 10:47:04 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            }
+        ]
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname),
+        },
+        compress: true,
+        port: 8080,
+        liveReload: true,
+        open: true,
+    },
+};
+
